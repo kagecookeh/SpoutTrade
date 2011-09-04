e3553608090c4b1cc2bc34b3867d2bd6b85a6cc8
Oliver Brown <oli.b1010@gmail.com>
2011-09-04 10:57:58 +0100

    Remove redundant directory

--- SpoutTrade/src/main/java/net/ark3l/SpoutTrade/GUI/TradeGUI.java
+++
@@ -1,5 +0,0 @@
-package net.ark3l.SpoutTrade.GUI;
-
-public class TradeGUI {
-//TODO - implement trade GUI
-}
--- SpoutTrade/src/main/java/net/ark3l/SpoutTrade/Trade/TradeState.java
+++
@@ -1,5 +0,0 @@
-package net.ark3l.SpoutTrade.Trade;
-
-public enum TradeState {
-	DEFAULT, CHEST_OPEN, CHEST_CLOSED, CONFIRMING, CONFIRMED
-}
