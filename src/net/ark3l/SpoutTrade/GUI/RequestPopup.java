e7cb4a79a97c98cb3246317b2f7cdab77b3b5c60
Oliver Brown <oli.b1010@gmail.com>
2011-09-05 14:54:30 +0100

    Committed missing class

---
+++ src/net/ark3l/SpoutTrade/GUI/RequestPopup.java
@@ -0,0 +1,46 @@
+package net.ark3l.SpoutTrade.GUI;
+
+/*   SpoutTrade - In game GUI trading for Bukkit Minecraft servers with Spout
+ Copyright (C) 2011  Oliver Brown (Arkel)
+
+ TileEntityVirtualChest and VirtualChest classes are attributed to Balor and
+ Timberjaw, the authors of GiftPost
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.getspout.spoutapi.gui.GenericLabel;
+import org.getspout.spoutapi.gui.WidgetAnchor;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+/**
+ * @author Oliver
+ */
+public class RequestPopup extends YesNoPopup {
+
+
+    public RequestPopup(SpoutPlayer sPlayer, String text) {
+        super(sPlayer);
+
+        GenericLabel label = new GenericLabel(text);
+        label.setAlign(WidgetAnchor.CENTER_CENTER);
+        label.setAnchor(WidgetAnchor.CENTER_CENTER);
+
+        popup.attachWidget(st, label);
+
+        sPlayer.getMainScreen().attachPopupScreen(popup);
+    }
+
+
+}
