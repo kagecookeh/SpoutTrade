07c7d81ed8fb390bfdd4ec7233c0d245c8af6928
Oliver Brown <oli.b1010@gmail.com>
2011-09-09 17:17:11 +0100

    Update copyright

--- src/net/ark3l/SpoutTrade/Inventory/TradeInventory.java
+++ src/net/ark3l/SpoutTrade/Inventory/TradeInventory.java
@@ -1,7 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  SpoutTrade - In game GUI trading for Bukkit Minecraft servers with Spout
+ * Copyright (C) 2011 Oliver Brown (Arkel)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * /
  */
+
 package net.ark3l.SpoutTrade.Inventory;
 
 import net.minecraft.server.EntityHuman;
