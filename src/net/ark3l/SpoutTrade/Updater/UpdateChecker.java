62508c93ce4de91d1b0130a87df45820788db227
Oliver Brown <oli.b1010@gmail.com>
2011-09-27 16:02:56 +0100

    Fix update checking

--- src/net/ark3l/SpoutTrade/Updater/UpdateChecker.java
+++ src/net/ark3l/SpoutTrade/Updater/UpdateChecker.java
@@ -87,13 +87,10 @@ public class UpdateChecker {
 			String[] version = bdd.getVersion().split(" ");
 			String[] versionNumbers = version[0].split(".");
 
-			int count = 0;
-			for(int i = 0; i < versionNumbers.length; i++) {
-				count += Integer.parseInt(versionNumbers[i]) * (4 - i);
-			}
-
-			if(!plugin.getDescription().getVersion().equalsIgnoreCase("v" + version[0])) {
+			if(!plugin.getDescription().getVersion().equalsIgnoreCase(version[0])) {
 				Log.warning("This version is out of date!");
+				Log.warning("This version: " + plugin.getDescription().getVersion());
+				Log.warning("Latest version: " + version[0]);
 
 				URL google = new URL(bdd.getLink());
 				ReadableByteChannel rbc = Channels.newChannel(google.openStream());
