2d445258ea0439ca0d16b9ffb6765434275809e9
Oliver Brown <oli.b1010@gmail.com>
2011-10-03 16:16:06 +0100

    Send a text based request notification to SpoutPlayers in case of GUI malfunction

--- src/net/ark3l/SpoutTrade/Trade/RequestPlayer.java
+++ src/net/ark3l/SpoutTrade/Trade/RequestPlayer.java
@@ -73,11 +73,12 @@ class RequestPlayer {
 	public void request(Player otherPlayer) {
 		if(this.player.isSpoutCraftEnabled()) {
 			requestPopup = new RequestPopup(player, ChatColor.RED + otherPlayer.getName() + ChatColor.WHITE + lang.getString(LanguageManager.Strings.REQUESTED));
-		} else {
-			getPlayer().sendMessage(ChatColor.RED + otherPlayer.getName() + "" + ChatColor.GREEN + lang.getString(LanguageManager.Strings.REQUESTED));
-			getPlayer().sendMessage(ChatColor.RED + "/trade accept" + ChatColor.GREEN + lang.getString(LanguageManager.Strings.TOACCEPT));
-			player.sendMessage(ChatColor.RED + "/trade decline" + ChatColor.GREEN + lang.getString(LanguageManager.Strings.TODECLINE));
 		}
+
+		getPlayer().sendMessage(ChatColor.RED + otherPlayer.getName() + "" + ChatColor.GREEN + lang.getString(LanguageManager.Strings.REQUESTED));
+		getPlayer().sendMessage(ChatColor.RED + "/trade accept" + ChatColor.GREEN + lang.getString(LanguageManager.Strings.TOACCEPT));
+		player.sendMessage(ChatColor.RED + "/trade decline" + ChatColor.GREEN + lang.getString(LanguageManager.Strings.TODECLINE));
+
 	}
 
 	/**
