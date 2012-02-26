fa241edb8ae1d9383c192c2bbc061b8e103ebae0
Oliver Brown <oli.b1010@gmail.com>
2012-02-26 10:28:30 +0000

    Don't save config on disable

--- src/main/java/net/ark3l/SpoutTrade/SpoutTrade.java
+++ src/main/java/net/ark3l/SpoutTrade/SpoutTrade.java
@@ -58,10 +58,6 @@ public class SpoutTrade extends JavaPlugin {
 
     public void onDisable() {
         manager.terminateActiveTrades();
-
-        languageManager.save();
-        config.save();
-
         Log.info(this + " disabled");
     }
 
